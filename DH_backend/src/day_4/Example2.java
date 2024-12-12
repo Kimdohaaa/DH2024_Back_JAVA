6286f7e405a0fd09510db831d88fca8fbe518e66
Kimdohaaa <jjeenee0408@gmail.com>
2024-12-12 11:36:06 +0900

    while문

--- DH_backend/src/day_4/Example2.java
+++ DH_backend/src/day_4/Example2.java
@@ -4,6 +4,18 @@ public class Example2 {
 
 	public static void main(String[] args) {
 		// while 문
+		/*
+		형식
+		while(조건식){
+		 	실행문; // 조건이 거짓 일 때 실행
+		};
+		실행문; 	// 조건이 참 일때 실행
+		
+		
+		사용할 변수에 초기값을 지정해주지 않을 경우 해당 변수를 이용한 연산 불가
+		-> 변수에 초기값을 지정해주지 않을 경우 변수에는 쓰레기값 / 임의값이 지정된다. => 임의값은 연산불가함
+		
+		*/
 		int num1 = 1;
 		int sum1 = 0;
 		
@@ -42,21 +54,27 @@ public class Example2 {
 		System.out.println("1 ~ 10 까지의 합은 " + sum3 + "입니다.");
 		
 		
-		// 구구단 2단 출력
+		// 예제 . 구구단 2단 출력
 		// 1. while 문
-		int num4 = 0;
-		int oper = 1;
+
+		int oper1 = 1;
 		
-		while (oper <= 9) {
-			num4 = oper * 2;
-			oper++;
-			System.out.println("2 * " + oper + " = " + num4);
-			
+		while (oper1 <= 9) {
+			System.out.println("2 * " + oper1 + " = " + (oper1 * 2));
+			oper1++;
 		}
+		
 		// 2. do - while 문
+		int oper2 = 1;
+		do {
+			System.out.println("2 * " + oper2 + " = " + (oper2 * 2));
+			oper2++;
+		}while(oper2 <= 9);
 			
-			
-	
+		// for 문
+		for(int oper3 = 1; oper3 <= 9; oper3 ++) {
+			System.out.println("2 * " + oper3 + " = " + (oper3 * 2));
+		}
 	}
 
 }
