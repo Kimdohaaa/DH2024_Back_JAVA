40771717a6be6e7ffe9ca15590a46a1cf82026bc
kimdohaaa <jjeenee0408@gmail.com>
2024-12-18 09:35:26 +0900

    배열

--- DH_backend/src/day_7/Example4.java
+++ DH_backend/src/day_7/Example4.java
@@ -1,6 +1,7 @@
 package day_7;
 
 import java.util.Arrays;
+import java.util.stream.Sink.ChainedInt;
 
 public class Example4 {
 	
@@ -43,9 +44,28 @@ public class Example4 {
 			System.out.println(sum);
 		}
 		
+		// 전체 배열 길이와 유효한 요소 값
+		double[] data = new double[5];	// double 5개를 저장할 수 있는 배열 선언
+		
+		data[0] = 10.0;
+		data[1] = 20.0;
+		data[2] = 30.0;
+		// 초기화 하지 않은 data[3],data[4]는 기본값 0.0으로 초기화된다.
+		
+		for(int i =0; i < data.length ; i++) {
+			System.out.println(data[i]);
+		}
 		
 		
 		
+		int 총점 = 0;
+		int 평균 = 0;
+		
+		int[] 점수 = new int[5];	
+		for(int i = 0; i < 점수.length -1 ; i++) {
+			총점 += 점수[i];
+			평균 *= 점수[i];
+		}
 	}	
 
 
