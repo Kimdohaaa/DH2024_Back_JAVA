d856954678987b3c9d04d46eab011c2a090b832c
kimdohaaa <jjeenee0408@gmail.com>
2024-12-19 12:08:12 +0900

    회원서비스_메소드

--- DH_backend/src/day_9/JoinService.java
+++ DH_backend/src/day_9/JoinService.java
@@ -6,6 +6,8 @@ public class JoinService {
 	void joinFunc(Scanner scan, Join[] joinArr) {
 		System.out.println(">> 회원가입을 선택하였습니다.");
 		
+		
+		// 매개변수로 받은 scan 을 통해 입력값을 변수에 대입
 		System.out.println("> 아이디 입력");
 		String id = scan.next();
 		System.out.println("> 비밀번호 입력");
@@ -13,11 +15,14 @@ public class JoinService {
 		System.out.println("> 닉네임 입력");
 		String nickName = scan.next();
 		
+		// Join 클래스 회원가입 객체 생성
 		Join p1 = new Join();
+		// 생성한 Join 클래스 객체에 입력값 대입
 		p1.ID = id;
 		p1.password = password;
 		p1.nickName = nickName;
 		
+		// joinArr[] 내 index 에 객체 저장
 		for(int i = 0; i <= joinArr.length -1; i++) {
 			if(joinArr[i] == null) {
 				joinArr[i] = p1;
@@ -28,21 +33,24 @@ public class JoinService {
 	void logInFunc(Scanner scan, Join[] joinArr) {
 		System.out.println(">> 로그인을 선택하였습니다.");
 		
+		// 매개변수로 받은 scan 을 통해 입력값을 변수에 대입
 		System.out.println("> 아이디 입력");
 		String id = scan.next();
 		System.out.println("> 비밀번호 입력");
 		String password = scan.next();
 		
+		// Join 클래스 로그인 객체 생성
 		Join l1 = new Join();
+		// 생성한 Join 클래스 객체에 입력 값 대입
 		l1.ID = id;
 		l1.password = password;
 		
 		for(int i = 0; i <= joinArr.length -1; i++) {
-			if(joinArr[i].ID.equals(l1.ID) && joinArr[i].password.equals(l1.password)) {
+			if(joinArr[i].ID.equals(l1.ID) && joinArr[i].password.equals(l1.password)) { // Join 클래스의 회원가입 객체의 값 == Join 클래스의 로그인 객체의 값
 				System.out.println(">> 로그인 성공");
-				System.out.println(joinArr[i].nickName + " 님 로그인 되었습니다.");
+				System.out.println(joinArr[i].nickName + " 님 로그인 되었습니다.");	// Join 클래스 회원가입 객체의 닉네임 출력
 				break;
-			}else {
+			}else {	// Join 클래스의 회원가입 객체의 값 != Join 클래스의 로그인 객체의 값
 				System.out.println(">> 로그인 실패");
 			}
 		}
