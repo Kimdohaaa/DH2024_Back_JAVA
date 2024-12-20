bcf5fafe20a436e87f47f178674edb9ce5864c1a
kimdohaaa <jjeenee0408@gmail.com>
2024-12-20 15:18:14 +0900

    회원서비스_메소드

---
+++ DH_backend/src/day_10/JoinService.java
@@ -0,0 +1,65 @@
+package day_10;
+
+import java.util.Scanner;
+
+public class JoinService {
+	void joinFunc(Scanner scan, Join[] joinArr, Join p) {
+		System.out.println(">> 회원가입 선택");
+		
+		System.out.println("> 아이디 입력");
+		String id = scan.next();
+		p.setId(id);
+		System.out.println("> 비밀번호 입력");
+		String password = scan.next();
+		p.setPassword(password);
+		System.out.println("> 닉네임 입력");
+		String nickName = scan.next();
+		p.setNickName(nickName);
+		
+		Join p1 = new Join(id,password,nickName);
+		System.out.println(p1);
+		
+		Boolean joinState = false;
+		for(int i = 0; i <= joinArr.length -1; i++) {
+			if(joinArr[i] == null) {
+				joinArr[i] = p1;
+				joinState = true;
+				break;
+			}
+		}
+		if(joinState == true) {
+			System.out.println(">> 회원가입 성공");
+		}else {
+			System.out.println(">> 회원가입 실패");
+		}
+	}
+	
+	void logInFunc(Scanner scan, Join[] joinArr, Join l) {
+		System.out.println(">> 로그인 선택");
+		
+		System.out.println("> 아이디 입력");
+		String id = scan.next();
+		l.setId(id);
+		System.out.println("> 비밀번호 입력");
+		String password = scan.next();
+		l.setPassword(password);
+		
+		Join l1 = new Join(id, password);
+		
+		
+		Boolean logInState = false;
+		for(int i = 0; i <= joinArr.length -1; i++) {
+			if(joinArr[i] != null && joinArr[i].getId().equals(l1.getId()) && joinArr[i].getPassword().equals(l1.getPassword())) {
+				System.out.println(">> 로그인 성공");
+				logInState = true;
+				break;
+			}
+			if(logInState == false) {
+				System.out.println(">> 로그인 실패");
+				break;
+			}
+			
+		}
+		
+	}
+}
