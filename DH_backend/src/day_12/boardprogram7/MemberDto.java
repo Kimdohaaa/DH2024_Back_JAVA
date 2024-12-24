1b2c0f61c8e2553a1ce6a94c5f2b736a9333a0e6
Kimdohaaa <jjeenee0408@gmail.com>
2024-12-24 12:32:38 +0900

    Member_클래스

---
+++ DH_backend/src/day_12/boardprogram7/MemberDto.java
@@ -0,0 +1,38 @@
+package day_12.boardprogram7;
+
+public class MemberDto {
+	// 멤버함수
+	private String id;
+	private String pwd;
+	private String name;
+	
+	// 생성자
+	MemberDto(){}
+	MemberDto(String id, String pwd, String name){
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+	}
+	
+	// getter / setter 메소드
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPw(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
