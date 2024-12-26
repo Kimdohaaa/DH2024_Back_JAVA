c3c23a3c32de9281e8c4f560a4a5030a12b1ac90
Kimdohaaa <jjeenee0408@gmail.com>
2024-12-26 16:00:55 +0900

    대기관리_main

--- DH_backend/src/day_13/example/AppStart.java
+++ DH_backend/src/day_13/example/AppStart.java
@@ -1,7 +1,15 @@
 package day_13.example;
 /*
-과제 : 대기명단 (MVC 패턴 사용) 
+과제 : 대기명단 (MVC 패턴 사용)
+- 전화번호와 인원수를 입력받아 대기등록 기능 구축
+- 현재 모든 대기명단을 출력하느 기능 구축 
 */
-public class AppStart {
 
+import day_13.example.view.WaitingView;
+
+public class AppStart {
+	public static void main(String[] args) {
+		// WaitingView -> getInstance() -> index() 접근
+		WaitingView.getInstance().index();
+	}
 }
