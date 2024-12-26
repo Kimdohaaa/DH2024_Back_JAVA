381d28fbbafec573ce8d012aaac8de55b5722081
Kimdohaaa <jjeenee0408@gmail.com>
2024-12-26 09:57:35 +0900

    MemberDto_클래스

--- DH_backend/src/day_12/MemberDto.java
+++ DH_backend/src/day_12/MemberDto.java
@@ -1,20 +1,15 @@
 package day_12;
 
-public class MemberDto {
+class MemberDto {
 	// 멤버변수
 	private String id;
 	private String pw;
-	private String[] rentalArr;
 	
 	// 생성자
 	MemberDto(){}
 	MemberDto(String id, String pw){
 		this.id = id;
 		this.pw = pw;
-	}
-	MemberDto(String[] rentalArr){
-		
-		this.rentalArr = rentalArr;
 	}
 	
 	// 메소드
@@ -30,21 +25,21 @@ public class MemberDto {
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
-	public String[] getRentalArr() {
-		return rentalArr;
-	}
-	public void setRentalArr(String[] rentalArr) {
+
+	
+}
+class RentalArr{
+	private String rentalArr;
+	
+	RentalArr(){};
+	RentalArr(String rentalArr){
 		this.rentalArr = rentalArr;
 	}
-	
-	public String rental(String[] rentalArr) {
-		String index = null;
-		for(int i = 0; i < rentalArr.length; i++) {
-			if(rentalArr[i] == null) {
-				rentalArr[i] = index;
-			}
-		}
-		return index;
+	public String getRentalArr() {
+		return rentalArr;
+	}
+	public void setRentalArr(String rentalArr) {
+		this.rentalArr = rentalArr;
 	}
 	
 
