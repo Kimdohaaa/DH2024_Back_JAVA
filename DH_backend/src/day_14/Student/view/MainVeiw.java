0b1e9776354cd3ce9cee296218c602c6d1743adb
Kimdohaaa <jjeenee0408@gmail.com>
2024-12-27 18:21:30 +0900

    학생관리_mainview

--- DH_backend/src/day_14/Student/view/MainVeiw.java
+++ DH_backend/src/day_14/Student/view/MainVeiw.java
@@ -1,5 +1,27 @@
 package day_14.Student.view;
 
-public class MainVeiw {
+import java.util.Scanner;
 
+public class MainVeiw {
+	// 싱글톤 s
+	private MainVeiw() {}
+	
+	private static MainVeiw instance = new MainVeiw() ;
+	
+	public static MainVeiw getInstance() {
+		return instance;
+	}
+	// 싱글톤 e
+	Scanner scan = new Scanner(System.in);
+	
+	public void run() {
+		System.out.println(">> 1. 학생 2. 수업 3. 수강신청");
+		int choose = scan.nextInt();
+		
+		if(choose == 1) {
+			
+		}else if(choose == 2 ) {
+			
+		}else if()
+	}
 }
