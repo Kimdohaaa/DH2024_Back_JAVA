3a039b37a6fb7ac70afcd90dce8fcefcb41d58c9
Kimdohaaa <jjeenee0408@gmail.com>
2024-12-27 17:50:59 +0900

    학생관리_DTO

---
+++ DH_backend/src/day_14/Student/model/dto/StudentDto.java
@@ -0,0 +1,35 @@
+package day_14.Student.model.dto;
+
+public class StudentDto {
+	private String name;
+	private String phone;
+	private int code;
+	
+	public StudentDto() {}
+	public StudentDto(String name, String phone, int code) {
+		this.name = name;
+		this.phone = phone;
+		this.code = code;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+	
+}
