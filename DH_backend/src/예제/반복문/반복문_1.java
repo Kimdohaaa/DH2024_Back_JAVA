029f2ffb0ec895ef678216cf34331e3ab63cc223
Kimdohaaa <jjeenee0408@gmail.com>
2024-12-27 09:18:13 +0900

    A+B-3

--- DH_backend/src/예제/반복문/반복문_1.java
+++ DH_backend/src/예제/반복문/반복문_1.java
@@ -1,4 +1,7 @@
 package 예제.반복문;
+
+import java.util.Scanner;
+
 /*
 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
@@ -10,8 +13,24 @@ package 예제.반복문;
 public class 반복문_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		while(true) {
+			
+			System.out.print(">> 케이스 개수 T : ");
+			int t = scan.nextInt();
 
+			
+			for(int i = 0; i < t ; i++) {
+				System.out.println(">> 정수 A :");
+				int a = scan.nextInt();
+				System.out.println(">> 정수 B :");
+				int b = scan.nextInt();
+				
+				System.out.println(a+b);
+			}
+			
+			
+		}
 	}
 
 }
