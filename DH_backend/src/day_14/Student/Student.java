96f87efe6a8c1ebeb6b1814e6c023b14ffc0719b
Kimdohaaa <jjeenee0408@gmail.com>
2024-12-27 17:50:30 +0900

    학생 관리_main

---
+++ DH_backend/src/day_14/Student/Student.java
@@ -0,0 +1,11 @@
+package day_14.Student;
+
+import day_14.Student.view.StudentView;
+
+public class Student {
+
+	public static void main(String[] args) {
+		StudentView.getInstance().run();
+	}
+
+}
