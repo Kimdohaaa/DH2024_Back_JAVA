1c63d94552cfebdaebd1cd4d556416002178f8fd
doha <jjeenee0408@gmail.com>
2024-12-28 14:10:45 +0900

    팀과제_학생관리시스템_MainView

--- DH_backend/src/day_14/Student/view/MainVeiw.java
+++ DH_backend/src/day_14/Student/view/MainVeiw.java
@@ -12,16 +12,22 @@ public class MainVeiw {
 		return instance;
 	}
 	// 싱글톤 e
+	
+	
 	Scanner scan = new Scanner(System.in);
 	
 	public void run() {
-		System.out.println(">> 1. 학생 2. 수업 3. 수강신청");
-		int choose = scan.nextInt();
-		
-		if(choose == 1) {
+		while(true) {
+			System.out.println(">> 1. 학생 2. 수업 3. 수강신청");
+			int choose = scan.nextInt();
 			
-		}else if(choose == 2 ) {
-			
-		}else if()
+			if(choose == 1) {
+				StudentView.getInstance().run();
+			}else if(choose == 2 ) {
+				// ClassView 연동
+			}else if(choose == 3 ) {
+				// AcceptView 연동
+			}
+		}
 	}
 }
