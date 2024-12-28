b6ed33b0b57813950e43ad23a26e65269738626e
doha <jjeenee0408@gmail.com>
2024-12-28 20:02:34 +0900

    개수 세기

--- DH_backend/src/예제/배열_1차원/배열_1.java
+++ DH_backend/src/예제/배열_1차원/배열_1.java
@@ -8,39 +8,36 @@ import java.util.Scanner;
 첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다. 둘째 줄에는 정수가 공백으로 구분되어져있다. 
 셋째 줄에는 찾으려고 하는 정수 v 가 주어진다. 입력으로 주어지는 정수와 v 는 -100보다 크거나 같으며, 100보다 작거나 같다.
 
-첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
+첫째 줄에 입력으로 주어진 N개의 정수 중에 v 가 몇 개인지 출력한다.
 */
 public class 배열_1 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println(">> 입력할 정수의 개수 : ");
-		int i = scan.nextInt();
+		System.out.print(">> 입력할 정수의 개수 : ");
+		int n = scan.nextInt();
 		
-		int find = 0;
-		int[] arr = new int[i];
+		int[] arr = new int[n];
 		int result = 0;
-				
-		for(int j = 0; j < i ; j++) {
-			System.out.println(">> 정수 : ");
-			int i1 = scan.nextInt();
+		
+		for(int index = 0; index < arr.length; index++) {
+			System.out.print(">> n 개의 정수 : ");
+			arr[index] = scan.nextInt();
 		}
 		
 		System.out.println(">> 탐색할 정수 : ");
-		find = scan.nextInt();
-		
-		for(int j = 0; j <= arr.length -1 ; i++) {
-			int findNum = arr[i];
-			if(find == findNum) {
+		int findi = scan.nextInt();
+		for(int index = 0; index < arr.length; index++) {
+			for(; findi == arr[index];) {
 				result++;
-				
 				break;
-			}	
+			}
 		}
-		
 		System.out.println(result);
 		
+		
+
 	}
 
 }
