e889e939fae4d8d74b8c54e8c5811e79250b3079
doha <jjeenee0408@gmail.com>
2024-12-28 14:09:13 +0900

    팀과제_학생관리시스템_main

--- DH_backend/src/day_14/Student/Student.java
+++ DH_backend/src/day_14/Student/Student.java
@@ -1,11 +1,12 @@
 package day_14.Student;
 
+import day_14.Student.view.MainVeiw;
 import day_14.Student.view.StudentView;
 
 public class Student {
 
 	public static void main(String[] args) {
-		StudentView.getInstance().run();
+		MainVeiw.getInstance().run();
 	}
 
 }
