a6fd8ece42bdd90103bd2b998c4891032b2d4f16
Kimdohaaa <jjeenee0408@gmail.com>
2024-12-30 16:11:23 +0900

    DB_테이블

--- DH_backend/src/database/DB테이블생성.sql
+++ DH_backend/src/database/DB테이블생성.sql
@@ -21,4 +21,37 @@ create table testtable2(필드명1 int, 필드명2 double);
 drop table testtable1;
 drop table if exists testtable2;
 
+# 다양한 데이터 타입을 이용한 테이블 생성 실습
+create table testtable3( # table s
+	# 테이블 내 속성명과 타입 선언 -> 여러 개가 들어갈 시 ,(쉼표) 로 구분
+    -- [1] 정수 타입
+    정수필드1 tinyint,
+    정수필드2 smallint,
+    정수필드3 mediumint,
+    정수필드4 int,
+    정수필드5 bigint,
+	-- [2] 실수타입
+    실수필드1 float,
+    실수필드2 double,
+    실수필드3  decimal,
+	-- [3] 시간 / 날짜 타입
+    날짜필드 date,
+    시간필드 time,
+    날짜시간필드 datetime,
+    -- [4] 문자타입
+    문자필드1 char(100),
+    문자필드2 varchar(100),
+    문자필드3 text,
+    문자필드4 longtext,
+	-- [5] 논리타입
+    논리필드 bool
+); # table e
+
+# 테이블 확인
+show tables;
+# 특정 테이블 확인
+select * from testtable3;
+
+
+
 
