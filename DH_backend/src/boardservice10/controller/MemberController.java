38fee146e7137a7dda3f132739ced9f4862752d1
minwoo817 <capzhang123@naver.com>
2025-01-07 15:22:40 +0900

    MemberController

--- DH_backend/src/boardservice10/controller/MemberController.java
+++ DH_backend/src/boardservice10/controller/MemberController.java
@@ -1,5 +1,36 @@
 package boardservice10.controller;
 
-public class MemberController {
+import boardservice10.model.dao.MemberDao;
+import boardservice10.model.dto.MemberDto;
 
+public class MemberController {
+	// 싱글톤 s
+	// [1] 생성자
+	private MemberController(){}
+	// [2] 객체 생성
+	private static MemberController instance = new MemberController();
+	// [3] 메소드
+	public static MemberController getInstance() {
+		return instance;
+	}
+	// 싱글톤 e
+	
+	// [1] 회원가입
+	public boolean signUp(MemberDto memberDto) {
+		boolean result = MemberDao.getInstance().signUp(memberDto);
+		
+		return result;
+	}
+	// [2] 로그인
+	public boolean read() {
+			
+	}
+	// [3] 아이디 찾기
+	public String update() {
+			
+	}
+	// [4] 비밀번호 찾기
+	public String delete() {
+			
+	}
 }
