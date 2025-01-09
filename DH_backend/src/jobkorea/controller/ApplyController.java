6db3930f35500c530976fb9d7cf4fadb0e9c20f6
minwoo817 <capzhang123@naver.com>
2025-01-09 15:04:14 +0900

    ApplyController

--- DH_backend/src/jobkorea/controller/ApplyController.java
+++ DH_backend/src/jobkorea/controller/ApplyController.java
@@ -12,4 +12,30 @@ public class ApplyController {
 		return instance;
 	}
 	// 싱글톤 e
+
+	// [1] 지원
+	public void applyC() {
+			
+	}
+	// [2] 지원 현황 출력
+	public void applyR() {
+			
+	}
+	// [3] 정보 수정 -> 비밀번호 / 이름 / 성별 / 생년월일 / 주소
+	public void applyU() {
+			
+	}
+	// [4] 지원 삭제 
+	public void applyD() {
+			
+	}
+		
+	// [1] 카테고리 리스트 출력
+	public void cList() {
+			
+	}
+	// [2] 공고 리스트 출력
+	public void pList() {
+			
+	}
 }
