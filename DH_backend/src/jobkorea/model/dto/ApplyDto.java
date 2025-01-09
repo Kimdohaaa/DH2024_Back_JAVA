785bb8b9072ac0112be7d727e276b69a7d025e28
minwoo817 <capzhang123@naver.com>
2025-01-09 14:07:53 +0900

    ApplyDto

--- DH_backend/src/jobkorea/model/dto/ApplyDto.java
+++ DH_backend/src/jobkorea/model/dto/ApplyDto.java
@@ -2,19 +2,58 @@ package jobkorea.model.dto;
 
 public class ApplyDto {
 	private boolean apass;
-
+	private int pno;
+	private int mno;
+	
 	public ApplyDto() {}
-	public boolean isApass() {
-		return apass;
+	
+
+	public ApplyDto(boolean apass, int pno, int mno) {
+		super();
+		this.apass = apass;
+		this.pno = pno;
+		this.mno = mno;
 	}
+
 
 	public void setApass(boolean apass) {
 		this.apass = apass;
 	}
-	
-	@Override
-	public String toString() {
-		return "ApplyDto [apass=" + apass + "]";
+
+
+	public boolean isApass() {
+		return apass;
 	}
 
+
+	public int getPno() {
+		return pno;
+	}
+
+
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+
+
+	public int getMno() {
+		return mno;
+	}
+
+
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ApplyDto [apass=" + apass + ", pno=" + pno + ", mno=" + mno + "]";
+	}
+
+
+	
+
+
+	
 }
