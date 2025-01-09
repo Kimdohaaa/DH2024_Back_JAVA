b7a2b5fcd0d5f412daf2fc019d99d195dc3c81f5
minwoo817 <capzhang123@naver.com>
2025-01-09 09:21:50 +0900

    MainController

---
+++ DH_backend/src/jobkorea/controller/MainController.java
@@ -0,0 +1,63 @@
+package jobkorea.controller;
+
+import jobkorea.model.dao.MainDao;
+import jobkorea.model.dto.PostDto;
+
+public class MainController {
+	// 싱글톤
+	// 생성자
+	private MainController() {}
+	// 객체생성
+	private static MainController instance = new MainController();
+	// 메소드
+	public static MainController getInstance() {
+		return instance;
+	}
+	// 싱글톤 e
+	
+	private int loginMno = 0;
+	private int loginEno = 0;
+	
+    // [1] 회원 회원가입 메소드
+    public void mSignUp() {
+       
+    }
+    // [2] 회원 로그인 메소드
+    public void mLogin() {
+       
+    }
+    // [3] 회원 로그아웃 메소드
+    public int mLogout() {
+    	loginMno = 0;
+    	
+    	return loginMno;
+    }
+    
+    
+    
+    // [1] 기업 회원가입 메소드
+    public void eSignUp() {
+       
+    }
+    // [2] 기업 로그인 메소드
+    public void eLogin() {
+       
+    }
+    // [3] 기업 로그아웃 메소드
+    public int eLogout() {
+    	loginEno = 0;
+    	
+    	return loginEno;
+    }
+    
+    
+    // [1] 우수기업 R
+    public void bestList(PostDto postDto) {
+    	MainDao.getInstance().bestList();
+	}
+    // [2] 후기 R
+    public void reviewList() {
+		
+	}
+}
+
