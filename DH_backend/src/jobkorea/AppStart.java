488b49a56ff4a650e905004f02cbff5f5963e72f
minwoo817 <capzhang123@naver.com>
2025-01-09 09:21:30 +0900

    AppStart

--- DH_backend/src/jobkorea/AppStart.java
+++ DH_backend/src/jobkorea/AppStart.java
@@ -1,10 +1,12 @@
 package jobkorea;
 
+import jobkorea.view.MainView;
+
 public class AppStart {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MainView.getInstance().run();
 	}
 
+	
 }
