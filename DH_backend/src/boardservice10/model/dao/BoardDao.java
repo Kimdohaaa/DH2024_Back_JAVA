a05ab5542fd81cb6c479715b7cc36e2db01e51db
minwoo817 <capzhang123@naver.com>
2025-01-10 15:54:56 +0900

    DAO

--- DH_backend/src/boardservice10/model/dao/BoardDao.java
+++ DH_backend/src/boardservice10/model/dao/BoardDao.java
@@ -28,20 +28,21 @@ public class BoardDao extends Dao {
 	// [2] 게시물 상세 보기
 	public BoardDto findById(int bno) {
 		try {
-			String sql = "select * from board where bno = ?";
-			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setInt(1, bno);
-				
+			String sql = "select b.* , c.cname , m.mid "
+					+ " from board as b inner join category as c on b.cno = c.cno "
+					+ " inner join member as m on b.mno = m.mno "
+					+ " where b.bno = ? ";
+			PreparedStatement ps = conn.prepareStatement( sql );
+			ps.setInt( 1 , bno );
 			ResultSet rs = ps.executeQuery();
-			if(rs.next()) {
-				int Bno = rs.getInt("bno");
-				String btitle = rs.getString("btitle");
-				String bcontent = rs.getString("bcontent");
-				int bview = rs.getInt("bview");
-				String bdate = rs.getString("bdate");
-				int cno = rs.getInt("cno");
-					
-				BoardDto boardDto = new BoardDto(Bno, btitle,bcontent,bview,bdate,cno);
+			if( rs.next() ) {
+				BoardDto boardDto = new BoardDto(
+						rs.getInt("bno") , rs.getString("btitle") , 
+						rs.getString("bcontent") , rs.getInt("bview") ,
+						rs.getString("bdate") , rs.getInt("mno"),
+						rs.getInt("cno" ));
+				boardDto.setCname( rs.getString("cname" ) );
+				boardDto.setMid( rs.getString("mid") );
 				
 				return boardDto;
 			}
@@ -62,26 +63,27 @@ public class BoardDao extends Dao {
 					
 		try {
 		
-			String sql = "select * from board";
+			String sql = "select b.* , c.cname , m.mid "
+					+ " from board as b inner join category as c on b.cno = c.cno "
+					+ " inner join member as m on b.mno = m.mno";
 			PreparedStatement ps = conn.prepareStatement(sql);
-			
 			ResultSet rs = ps.executeQuery();
-			
-			// 전체 조회를 위해 while 문 사용
-			while(rs.next()) {
-				// 1) 현재 레코드의 각 속성별 값 반환하기
-				 int bno = rs.getInt("bno");
-				 String btitle = rs.getString("btitle");
-				 String bcontent = rs.getString("bcontent");
-				 int bview = rs.getInt("bview");
-				 String bdate = rs.getString("bdate");
-				 int cno = rs.getInt("cno");
-				 
-				 // 2) 객체화
-				 BoardDto boardDto = new BoardDto(bno, btitle,bcontent,bview,bdate,cno);
-				 
-				 // 3) ArraysList 에 삽입
-				 list.add(boardDto);
+			while( rs.next() ) {
+				// 레코드 : DB테이블의 가로/행 데이터	// 조회된 결과 레코드1개 <---> 인스턴스(DTO) 1개 // 레코드 여러개 <---> [] 1개 또는 ArrayList 1개 
+				// 1. 현재 레코드의 각 속성별 값 반환하기 
+				int bno = rs.getInt("bno");
+				String btitle = rs.getString("btitle");
+				String bcontent = rs.getString("bcontent");
+				int bview = rs.getInt("bview");
+				String bdate = rs.getString("bdate");
+				int mno = rs.getInt("mno");
+				int cno = rs.getInt("cno" );
+				BoardDto boardDto = new BoardDto(bno, btitle, bcontent, bview, bdate, mno, cno); // 2. 반환 속성값들을 dto(객체)로 만들기 
+				
+				boardDto.setCname( rs.getString("cname" ) );
+				boardDto.setMid( rs.getString("mid") );
+				
+				list.add(boardDto); // 3. 생성된 dto(객체)를 리스트에 담기 
 				 
 			}
 	
