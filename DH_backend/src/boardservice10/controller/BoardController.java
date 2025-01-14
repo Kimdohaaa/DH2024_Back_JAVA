e05ad6728159fe7d10fb314436826aa1b07d0fb9
minwoo817 <capzhang123@naver.com>
2025-01-14 12:23:03 +0900

    BoardController

--- DH_backend/src/boardservice10/controller/BoardController.java
+++ DH_backend/src/boardservice10/controller/BoardController.java
@@ -14,17 +14,28 @@ public class BoardController {
 	}
 	// 싱글톤 e
 	
-	// [1] 게시물 쓰기
-	public void name() {
-			
+	// 카테고리전체조회
+	public ArrayList<BoardDto> categoryAll(){
+		ArrayList<BoardDto> result = BoardDao.getInstance().categoryAll();
+		
+		return result;
 	}
-	// [2] 게시물 상세 보기
+	
+	// 게시물 쓰기
+	public boolean write(BoardDto boardDto) {
+		int loginMno = MemberController.getInstance().getLogInMno();
+		boardDto.setMno(loginMno);
+		boolean result = BoardDao.getInstance().write(boardDto);
+		
+		return result;
+	}
+	// 게시물 상세 보기
 	public BoardDto findById(int bno) {
 		BoardDto boardDto = BoardDao.getInstance().findById(bno);
 		
 		return boardDto;
 	}
-	// [3] 게시물 전체 조회
+	// 게시물 전체 조회
 	public ArrayList<BoardDto> findAll() {
 		ArrayList<BoardDto> result = BoardDao.getInstance().findAll();
 		
