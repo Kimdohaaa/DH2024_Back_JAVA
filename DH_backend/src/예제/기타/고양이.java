530bae11c511aa57cb336869d4a5e7857e69b01f
Kimdohaaa <jjeenee0408@gmail.com>
2025-01-21 09:26:52 +0900

    고양이

---
+++ DH_backend/src/예제/기타/고양이.java
@@ -0,0 +1,14 @@
+package 예제.기타;
+
+// 예제와 같이 고양이 출력하기
+public class 고양이 {
+
+	public static void main(String[] args) {
+		System.out.println("\\     /\\");
+		System.out.println(" )   ( ')");
+		System.out.println("(   /  )");
+		System.out.println(" \\ (__)|");
+		
+	}
+
+}
